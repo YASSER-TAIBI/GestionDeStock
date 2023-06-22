7654405fc32cfbd041f56926b63b1194217d35f4
YASSER-TAIBI <yasser.taibi.19@gmail.com>
2023-06-22 20:27:15 +0200

    Finalisation de controleur 'Entreprise'

--- src/main/java/com/yazzer/gestiondestock/controller/api/EntrepriseApi.java
+++ src/main/java/com/yazzer/gestiondestock/controller/api/EntrepriseApi.java
@@ -1,27 +1,26 @@
 package com.yazzer.gestiondestock.controller.api;
 
 import com.yazzer.gestiondestock.dto.EntrepriseDto;
+import io.swagger.annotations.Api;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.yazzer.gestiondestock.utils.Constants.APP_ROOT;
+import static com.yazzer.gestiondestock.utils.Constants.ENTREPRISE_ENDPOINT;
 
+@Api(ENTREPRISE_ENDPOINT)
 public interface EntrepriseApi {
 
-    @PostMapping(value = APP_ROOT + "/entreprise/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    EntrepriseDto save (EntrepriseDto dto);
+    @PostMapping(value = ENTREPRISE_ENDPOINT +"/create")
+    EntrepriseDto save (@RequestBody EntrepriseDto dto);
 
-    @GetMapping(value = APP_ROOT + "/entreprise/{idEntreprise}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = ENTREPRISE_ENDPOINT + "/{idEntreprise}")
     EntrepriseDto findById(@PathVariable("idEntreprise") Integer id);
 
-    @GetMapping(value = APP_ROOT + "/entreprise/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = ENTREPRISE_ENDPOINT + "/all")
     List<EntrepriseDto> findALL();
 
-    @DeleteMapping(value = APP_ROOT + "/entreprise/delete/{idEntreprise}")
+    @DeleteMapping(value = ENTREPRISE_ENDPOINT + "/delete/{idEntreprise}")
     void delete(@PathVariable("idEntreprise") Integer id);
 }
--- src/main/java/com/yazzer/gestiondestock/utils/Constants.java
+++ src/main/java/com/yazzer/gestiondestock/utils/Constants.java
@@ -5,4 +5,5 @@ public interface Constants {
     String APP_ROOT = "gestiondestock/v1";
 
     String COMMANDE_FOURNISSEUR_ENDPOINT = APP_ROOT + "/commandesfournisseurs";
+    String ENTREPRISE_ENDPOINT = APP_ROOT + "/entreprises";
 }
